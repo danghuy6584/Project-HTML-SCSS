f4398db862c46db1e665b7c039185f136b392cd9
Van Huy <danghuy6584@gmail.com>
2024-08-14 02:11:38 +0700

    Add react-router-dom package and update dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import "./App.css";
 import SlideImage from "./Components/3DSlide/SlideImage";
+import Teleport from "./Components/Teleport/Teleport";
 
 function App() {
   return (
-    <div className="App">
-      <SlideImage />
-    </div>
+    <Router>
+      <div className="App">
+        <Outlet />
+      </div>
+      <Routes>
+        <Route exact path="/" element={<SlideImage />} />
+        <Route path="/teleport" element={<Teleport />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Components/Teleport/Teleport.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Teleport() {
+  return <div>Teleport</div>;
+}
+
+export default Teleport;
